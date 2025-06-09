5e88194b12f8a336ed12af2c16dbd47bbed46b5e
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-09 18:57:05 +0700

    melempar sebuah interger

--- manualException.cpp
+++ manualException.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 int main()
+{
+    try 
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo"); // Melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
