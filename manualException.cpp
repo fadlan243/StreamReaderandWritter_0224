d68234f6d683224b21f76ec2a59dee4b06d9f407
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-09 18:59:42 +0700

    blok ini akan dieksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -6,6 +6,12 @@ int main()
     try 
     {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw("halo"); // Melemparkan sebuah integer maka
+        throw("halo"); // Melemparkan sebuah interger maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    
